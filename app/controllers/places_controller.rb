130b37ae117e7c22aaa931b5a126a3cad1828388
Devin Crossman <devin.crossman2@gmail.com>
2019-07-20 20:02:47 +0000

    changed permissions for user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create] 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy] 
   
   def index
     @places = Place.paginate(:page => params[:page], per_page: 4)
@@ -10,15 +10,44 @@ class PlacesController < ApplicationController
   end
 
 # function that controls button
-  def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+def create
+  @place = current_user.places.create(place_params)
+  redirect_to root_path
+end
+
+def show
+  @place=Place.find(params[:id])
+end
+
+def edit
+    @place= Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'You must be the creator to edit this', status: forbidden
+    end
+end
+
+def update
+  @place= Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: forbidden
   end
+  @place.update_attributes(place_params)
+  redirect_to root_path
+end
+
+def destroy
+  @place= Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: forbidden
+  end
+  @place.destroy
+  redirect_to root_path
+end
 
   private
 
   def place_params
-    params.require(:palce).permit(:name, :description, :address)
+    params.require(:place).permit(:name, :description, :address)
   end
 
 end
